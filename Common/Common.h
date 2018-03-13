ed76d51f09e13d3bb9bd1ff1cc9c735976aa3c7a
Janusz Janik <janusz.janik@hotmail.com>
2018-03-13 23:06:01 +0100

    project reorganized

---
+++ Common/Common.h
@@ -0,0 +1,12 @@
+#define VK_NO_PROTOTYPES
+
+#include "vulkan\vulkan.h"
+#include <iostream>
+
+#ifdef _WIN32
+#include <Windows.h>
+#define LIBRARY_TYPE HMODULE
+#elif defined __linux
+#include <dlfcn.h>
+#define LIBRARY_TYPE void*
+#endif
